2d881c15b328e825215588e40267819250540c19
Semen <ch-semen@rambler.ru>
2025-01-25 13:34:24 +0300

    p218

--- server/src/main.ts
+++ server/src/main.ts
@@ -36,3 +36,96 @@ app.get("/mailboxes",
         }
     }
 );
+
+app.get("/mailboxes/:mailbox",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const messages: IMAP.IMessage[] = await imapWorker.listMessages({
+                mailbox: inRequest.params.mailbox
+            });
+            inResponse.json(messages);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.get("/messages/:mailbox/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const messageBody: string | undefined = await imapWorker.
+                getMessageBody({
+                    mailbox: inRequest.params.mailbox,
+                    id: parseInt(inRequest.params.id, 10)
+                });
+            inResponse.send(messageBody);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/messages/:mailbox/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            await imapWorker.deleteMessage({
+                mailbox: inRequest.params.mailbox,
+                id: parseInt(inRequest.params.id, 10)
+            });
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.post("/messages",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const smtpWorker: SMTP.Worker = new SMTP.Worker(serverInfo);
+            await smtpWorker.sendMessage(inRequest.body);
+            inResponse.send("ok");
+        } catch (inError) { }
+        inResponse.send("error");
+    }
+);
+
+app.get("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contacts: IContact[] = await contactsWorker.listContacts();
+            inResponse.json(contacts);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.post("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contact: IContact = await contactsWorker.
+                addContact(inRequest.body);
+            inResponse.json(contact);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/contacts/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            await contactsWorker.deleteContact(inRequest.params.id);
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);        
