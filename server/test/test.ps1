19eb390a513dc091ae349e0dc518c21c7cc63e95
Semen <ch-semen@rambler.ru>
2025-01-27 23:09:23 +0300

    server tests2

--- server/test/test.ps1
+++ server/test/test.ps1
@@ -15,16 +15,17 @@ curl -inFile "testNewMessage.json" -Headers @{"Content-Type"="application/json"}
 
 curl localhost/contacts
 # curl -d @newContact.json -H "Content-Type:application/json" -X POST localhost/contacts
-rem curl localhost/contacts/v3kIznMU5quHrZgZ
-curl -d @newContact2.json -H "Content-Type:application/json" -X PUT localhost/contacts/v3kIznMU5quHrZgZ
-curl -d @newContact.json -H "Content-Type:application/json" -X PUT localhost/contacts/v3kIznMU5quHrZgZ
-curl -d @newContact.json -H "Content-Type:application/json" -method PUT localhost/contacts/v3kIznMU5quHrZgZ
+# rem curl localhost/contacts/v3kIznMU5quHrZgZ
+# curl -d @newContact2.json -H "Content-Type:application/json" -X PUT localhost/contacts/v3kIznMU5quHrZgZ
+# curl -d @newContact.json -H "Content-Type:application/json" -X PUT localhost/contacts/v3kIznMU5quHrZgZ
+# curl -d @newContact.json -H "Content-Type:application/json" -method PUT localhost/contacts/v3kIznMU5quHrZgZ
 
 curl localhost/contacts | select -expand Content | ConvertFrom-Json
 curl -inFile "newContact.json" -Headers @{"Content-Type"="application/json"} -method POST localhost/contacts/
-
 $c=curl localhost/contacts | select -expand Content | ConvertFrom-Json
 $id = $c | select -expand _id | select -first 1
+$id
+
 curl -inFile "newContact2.json" -Headers @{"Content-Type"="application/json"} -method PUT localhost/contacts/$id
 curl -method DELETE localhost/contacts/$id
 curl localhost/contacts
